2cb854318203d8ad2156057143966310ddfb9763
Kunal-Kamod25 <kunalkamod3@gmail.com>
2026-03-21 23:10:28 +0530

    chore(security): update frontend/src/lib/store.js

--- autojudge/frontend/src/lib/store.js
+++ autojudge/frontend/src/lib/store.js
@@ -3,14 +3,12 @@ import { persist } from 'zustand/middleware';
 
 export const useAuthStore = create(persist((set) => ({
   user: null,
-  accessToken: null,
   isLoading: false,
   setUser: (user) => set({ user }),
-  setToken: (accessToken) => set({ accessToken }),
   setLoading: (isLoading) => set({ isLoading }),
-  login: (user, accessToken) => set({ user, accessToken }),
-  logout: () => set({ user: null, accessToken: null }),
-}), { name: 'auth-store', partialize: (s) => ({ user: s.user, accessToken: s.accessToken }) }));
+  login: (user) => set({ user }),
+  logout: () => set({ user: null }),
+}), { name: 'auth-store', partialize: (s) => ({ user: s.user }) }));
 
 export const useSubmissionStore = create((set) => ({
   currentSubmission: null,
